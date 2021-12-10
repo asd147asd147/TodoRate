ebd7a23be6e5ffc6a5fcfc694aa3e14d37e23514
asd147asd147 <asd147asd147@naver.com>
2021-12-10 23:17:12 +0900

    Maintenance code

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,42 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Todos {
-  List<Todo> todo = [];
+class TodoList {
+    List<Todo> todos = [];
+    int get length => todos.length;
 
-  Todos();
-  Todos.fromJson(Map<String, dynamic> json) {
-    if (json['todo'] != null) {
-      todo = [];
-      json['todo'].forEach((v) {
-        todo.add(new Todo.fromJson(v));
-      });
-    }
-  }
+    TodoList();
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.todo != null) {
-      data['todo'] = this.todo.map((v) => v.toJson()).toList();
+    void addItem(String name) {
+        todos.add(new Todo(name));
     }
-    return data;
-  }
+
+    void removeItem(int index) {
+        todos.removeAt(index);
+    }
+
+    TodoList.fromJson(Map<String, dynamic> json) {
+        if (json['todo'] != null) {
+            todos = [];
+            json['todo'].forEach((v) {
+                todos.add(new Todo.fromJson(v));
+            });
+        }
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        if (this.todos != null) {
+            data['todo'] = this.todos.map((v) => v.toJson()).toList();
+        }
+        return data;
+    }
 }
 
 class Todo {
-  String name = '';
-  bool isChecked = false;
+    String name = '';
+    bool isChecked = false;
+    String key = UniqueKey().toString();
 
-  Todo({required String this.name});
+    Todo(String this.name);
 
-  Todo.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    isChecked = json['isChecked'];
-  }
+    Todo.fromJson(Map<String, dynamic> json) {
+        name = json['name'];
+        isChecked = json['isChecked'];
+    }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['name'] = this.name;
-    data['isChecked'] = this.isChecked;
-    return data;
-  }
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['name'] = this.name;
+        data['isChecked'] = this.isChecked;
+        return data;
+    }
 }
