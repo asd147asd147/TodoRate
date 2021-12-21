62a4406a1b2dd257c5d29a336d482b77274eb803
asd147asd147 <asd147asd147@naver.com>
2021-12-22 00:38:32 +0900

    Add todo button of category & Dismissible and Slider collaborate, but Dismissible area is error

--- lib/mainListView.dart
+++ lib/mainListView.dart
@@ -11,7 +11,7 @@ class Item {
         required this.expandedValue,
         required this.headerValue,
         this.itemValue = 0,
-        this.isExpanded = true,
+        this.isExpanded = false,
     });
 
     String expandedValue;
@@ -34,6 +34,7 @@ class _MainListViewState extends State<MainListView> {
 
     Widget _buildPanel() {
         return ExpansionPanelList(
+                //expandedHeaderPadding: const EdgeInsets.all(30.0),
                 expansionCallback: (int index, bool isExpanded) {
                     setState((){
                         _data[index].isExpanded = !isExpanded;
@@ -42,59 +43,67 @@ class _MainListViewState extends State<MainListView> {
                 children: _data.map<ExpansionPanel>((Item item) {
                     return ExpansionPanel(
                             headerBuilder: (BuildContext context, bool isExpanded) {
-                                return ListTile(
-                                        title: Text(item.headerValue),
+                                return Row(
+                                        children: [
+                                            Expanded(
+                                                    child: ListTile(
+                                                            title: Text(item.headerValue),
+                                                    ),
+                                            ),
+                                            IconButton(
+                                                    onPressed: () {},
+                                                    icon: Icon(Icons.add),
+                                            ),
+                                        ],
                                 );
                             },
                             body: _todoUnit(item),
-                            /*Dismissible(
-                                          background: Container(color : Colors.red,),
-                                          direction: DismissDirection.startToEnd,
-                                          onDismissed: (direction){
-                                              if(direction == DismissDirection.startToEnd){
-                                                  setState(() {
-                                                      _data.removeWhere((Item currentItem) => item == currentItem);
-                                                  });
-                                              }
-                                          },
-                                          child: _todoUnit(item),
-                                          key: Key(UniqueKey().toString()),
-                                  ),*/
                     isExpanded: item.isExpanded,
                     );
                 }).toList(),
                 );
     }
-    double _value = 0.0;
 
     Widget _todoUnit(Item item){
-        return Column(
-                children: [
-                    ListTile(
-                            title: Text(item.expandedValue),
-                            subtitle: const Text('To delete this panel'),
-                    ),
-                    SliderTheme(
-                            data: SliderTheme.of(context).copyWith(
-                                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 4.0),
-                                    overlayShape: RoundSliderOverlayShape(overlayRadius: 14.0),
-                            ),
-                            child : Slider(
-                                    value: _value,
-                                    max: 100,
-                                    divisions: 100,
-                                    label: _value.round().toString(),
-                                    onChanged: (double value){
+        return Container(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                        children: [ 
+                        Dismissible(
+                                background: Container(color : Colors.red,),
+                                direction: DismissDirection.startToEnd,
+                                onDismissed: (direction){
+                                    if(direction == DismissDirection.startToEnd){
                                         setState(() {
-                                            _value = value;
+                                            _data.removeWhere((Item currentItem) => item == currentItem);
                                         });
-                                    },
-                                    onChangeEnd: (double value){
-                                        print(value);
                                     }
-                            ),
-                    ),
-                    ]);
+                                },
+                                child: ListTile(
+                                               title: Text(item.expandedValue),
+                                               subtitle: const Text('To delete this panel'),
+                                       ),
+                                key: Key(UniqueKey().toString()),
+                        ),
+                        SliderTheme(
+                                data: SliderTheme.of(context).copyWith(
+                                        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 4.0),
+                                        overlayShape: RoundSliderOverlayShape(overlayRadius: 14.0),
+                                ),
+                                child : Slider(
+                                        value: item.itemValue,
+                                        max: 100,
+                                        divisions: 20,
+                                        label: item.itemValue.round().toString(),
+                                        onChanged: (double value){
+                                            setState(() {
+                                                item.itemValue = value;
+                                            });
+                                        },
+                                ),
+                        ),
+                        ]),
+                        );
     }
 
 
